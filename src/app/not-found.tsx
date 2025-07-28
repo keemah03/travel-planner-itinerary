35d15c95cc6236f87ccc71cda299261f27aa6f69
keemah03 <hikmahopeyemi016@gmail.com>
2025-07-28 14:20:13 +0100

    Remove redundant message from NotFoundPage component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,9 +8,6 @@ function NotFoundPage() {
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
         404 - Page Not Found
       </h1>
-      <p className="text-lg text-gray-600">
-        Sorry, the page you are looking for does not exist.
-      </p>
       <p className="text-lg text-gray-600 mt-4">
         Go back&nbsp;
         <Link
